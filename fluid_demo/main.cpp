5068dfd86502db71ea605ce16bd4ac208a4829fe
Toh Yong Cheng <tohyongcheng@gmail.com>
2015-04-14 00:34:06 +0800

    do 3d

--- fluid_demo/main.cpp
+++ fluid_demo/main.cpp
@@ -1,41 +1,26 @@
-/*
- ======================================================================
- demo.c --- protoype to show off the simple solver
- ----------------------------------------------------------------------
- Author : Jos Stam (jstam@aw.sgi.com)
- Creation Date : Jan 9 2003
- 
- Description:
- 
-	This code is a simple prototype that demonstrates how to use the
-	code provided in my GDC2003 paper entitles "Real-Time Fluid Dynamics
-	for Games". This code uses OpenGL and GLUT for graphics and interface
- 
- =======================================================================
- */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <GLUT/glut.h>
 #include "solver.c"
 /* macros */
 
-#define IX(i,j) ((i)+(N+2)*(j))
+#define IX(i,j,k) ((i)+(N+2)*(j) + (N+2)*(N+2) *(k))
 
 /* external definitions (from solver.c) */
 
-extern void dens_step ( int N, float * x, float * x0, float * u, float * v, float diff, float dt );
-extern void vel_step ( int N, float * u, float * v, float * u0, float * v0, float visc, float dt );
-extern void temp_step ( int N, float * u, float * v, float * u0, float * v0, float visc, float dt );
+extern void dens_step ( int N, float * x, float * x0, float * u, float * v, float * w, float diff, float dt );
+extern void temp_step ( int N, float * u, float * v, float * w, float diff, float dt );
+extern void vel_step ( int N, float * u, float * v, float * w, float * u0, float * v0, float* w0, float visc, float dt );
+
 
 /* global variables */
 
 static int N;
 static float dt, diff, visc;
 static float force, source, temp_source;
-static int dvel;
+static int dvel, dAxis, addsource;
 
-static float * u, * v, * u_prev, * v_prev;
+static float * u, * v, * u_prev, * v_prev, * w, * w_prev;
 static float * dens, * dens_prev;
 static float * temp, * temp_prev;
 static float * buo;
@@ -44,6 +29,12 @@ static int win_id;
 static int win_x, win_y;
 static int mouse_down[3];
 static int omx, omy, mx, my;
+static float source_alpha =  0.05; //for displaying density
+
+GLfloat trans[3];
+GLfloat rot[2];
+
+
 
 
 /*
@@ -52,43 +43,52 @@ static int omx, omy, mx, my;
  ----------------------------------------------------------------------
  */
 
+float clamp(float x) {
+    return x > 360.0f ? x-360.0f : x < -360.0f ? x+=360.0f : x;
+}
 
 static void free_data ( void )
 {
     if ( u ) free ( u );
     if ( v ) free ( v );
+    if ( w ) free ( w );
     if ( u_prev ) free ( u_prev );
     if ( v_prev ) free ( v_prev );
+    if ( w_prev ) free ( w_prev );
     if ( dens ) free ( dens );
     if ( dens_prev ) free ( dens_prev );
     if ( temp ) free (temp);
     if ( temp_prev ) free ( temp_prev );
+    if ( buo ) free ( buo );
 }
 
 static void clear_data ( void )
 {
-    int i, size=(N+2)*(N+2);
+    int i, size=(N+2)*(N+2)*(N+2);
     
     for ( i=0 ; i<size ; i++ ) {
-        u[i] = v[i] = u_prev[i] = v_prev[i] = dens[i] = dens_prev[i] = temp[i] = temp_prev[i] = 0.0f;
+        u[i] = v[i] = w[i] = u_prev[i] = v_prev[i] = w_prev[i] = dens[i] = dens_prev[i] = buo[i] = 0.0f;
+        temp[i] = temp_prev[i] = 0;
     }
 }
 
 static int allocate_data ( void )
 {
-    int size = (N+2)*(N+2);
+    int size = (N+2)*(N+2)*(N+2);
     
     u			= (float *) malloc ( size*sizeof(float) );
     v			= (float *) malloc ( size*sizeof(float) );
+    w			= (float *) malloc ( size*sizeof(float) );
     u_prev		= (float *) malloc ( size*sizeof(float) );
     v_prev		= (float *) malloc ( size*sizeof(float) );
+    w_prev		= (float *) malloc ( size*sizeof(float) );
     dens		= (float *) malloc ( size*sizeof(float) );
     dens_prev	= (float *) malloc ( size*sizeof(float) );
     temp        = (float *) malloc ( size*sizeof(float) );
     temp_prev   = (float *) malloc ( size*sizeof(float) );
-    buo   = (float *) malloc ( size*sizeof(float) );
+    buo         = (float *) malloc ( size*sizeof(float) );
     
-    if ( !u || !v || !u_prev || !v_prev || !dens || !dens_prev || !temp || !temp_prev ) {
+    if ( !u || !v || !w || !u_prev || !v_prev || !w_prev || !dens || !dens_prev || !temp || !temp_prev || !buo ) {
         fprintf ( stderr, "cannot allocate data\n" );
         return ( 0 );
     }
@@ -103,25 +103,29 @@ static int allocate_data ( void )
  ----------------------------------------------------------------------
  */
 
-static void pre_display ( void )
-{
-    glViewport ( 0, 0, win_x, win_y );
-    glMatrixMode ( GL_PROJECTION );
-    glLoadIdentity ();
-    gluOrtho2D ( 0.0, 1.0, 0.0, 1.0 );
-    glClearColor ( 0.0f, 0.0f, 0.0f, 1.0f );
-    glClear ( GL_COLOR_BUFFER_BIT );
-}
-
-static void post_display ( void )
-{
-    glutSwapBuffers ();
+static void draw_axis( void ) {
+    glLineWidth ( 1.0f );
+    glBegin (GL_LINES);
+    
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f (0.25f, 0.f, 0.25f);
+    glVertex3f (1.0f, 0.f, 0.25f);
+    
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f (0.25f, 0.f, 0.25f);
+    glVertex3f (0.25f, 1.0f, 0.25f);
+    
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f (0.25f, 0.f, 0.25f);
+    glVertex3f (0.25f, 0.f, 1.0f);
+    
+    glEnd();
 }
 
 static void draw_velocity ( void )
 {
-    int i, j;
-    float x, y, h;
+    int i, j, k;
+    float x, y, z, h;
     
     h = 1.0f/N;
     
@@ -134,9 +138,11 @@ static void draw_velocity ( void )
         x = (i-0.5f)*h;
         for ( j=1 ; j<=N ; j++ ) {
             y = (j-0.5f)*h;
-            
-            glVertex2f ( x, y );
-            glVertex2f ( x+u[IX(i,j)], y+v[IX(i,j)] );
+            for (k=1; k <= N ; k++) {
+                z =(k-0.5)*h;
+                glVertex3f ( x, y, z );
+                glVertex3f ( x+u[IX(i,j,k)], y+v[IX(i,j,k)], z+v[IX(i,j,k)] );
+            }
         }
     }
     
@@ -145,56 +151,126 @@ static void draw_velocity ( void )
 
 static void draw_density ( void )
 {
-    int i, j;
-    float x, y, h, d00, d01, d10, d11;
+    int i, j, k;
+    float x, y, z, h, d000, d010, d100, d110, d001, d011, d101, d111;;
     
     h = 1.0f/N;
     
     glBegin ( GL_QUADS );
     
-    for ( i=0 ; i<=N ; i++ ) {
+    for ( i=0; i<=N; i++ ) {
         x = (i-0.5f)*h;
-        for ( j=0 ; j<=N ; j++ ) {
+        for ( j=0; j<=N; j++ ) {
             y = (j-0.5f)*h;
-            
-            d00 = dens[IX(i,j)];
-            d01 = dens[IX(i,j+1)];
-            d10 = dens[IX(i+1,j)];
-            d11 = dens[IX(i+1,j+1)];
-            
-            glColor3f ( d00, d00, d00 ); glVertex2f ( x, y );
-            glColor3f ( d10, d10, d10 ); glVertex2f ( x+h, y );
-            glColor3f ( d11, d11, d11 ); glVertex2f ( x+h, y+h );
-            glColor3f ( d01, d01, d01 ); glVertex2f ( x, y+h );
+            for ( k=0; k<=N; k++ ) {
+                z = (k-0.5f)*h;
+                
+                d000 = dens[IX(i,j,k)];
+                d010 = dens[IX(i,j+1,k)];
+                d100 = dens[IX(i+1,j,k)];
+                d110 = dens[IX(i+1,j+1,k)];
+                
+                d001 = dens[IX(i,j,k+1)];
+                d011 = dens[IX(i,j+1,k+1)];
+                d101 = dens[IX(i+1,j,k+1)];
+                d111 = dens[IX(i+1,j+1,k+1)];
+                
+                // draw density as a cube of quads
+                
+                glColor4f ( d111, d111, d111, source_alpha ); glVertex3f ( x+h,y+h,z+h );
+                glColor4f ( d011, d011, d011, source_alpha ); glVertex3f ( x, y+h, z+h);
+                glColor4f ( d001, d001, d001, source_alpha ); glVertex3f ( x, y, z+h );
+                glColor4f ( d101, d101, d101, source_alpha ); glVertex3f ( x+h, y, z+h );
+                
+                glColor4f ( d110, d110, d110, source_alpha ); glVertex3f ( x+h, y+h, z );
+                glColor4f ( d111, d111, d111, source_alpha ); glVertex3f ( x+h,y+h,z+h );
+                glColor4f ( d101, d101, d101, source_alpha ); glVertex3f ( x+h, y, z+h );
+                glColor4f ( d100, d100, d100, source_alpha ); glVertex3f ( x+h, y, z );
+                
+                glColor4f ( d010, d010, d010, source_alpha ); glVertex3f ( x, y+h, z );
+                glColor4f ( d110, d110, d110, source_alpha ); glVertex3f ( x+h, y+h, z );
+                glColor4f ( d100, d100, d100, source_alpha ); glVertex3f ( x+h, y, z );
+                glColor4f ( d000, d000, d000, source_alpha ); glVertex3f ( x, y, z );
+                
+                glColor4f ( d011, d011, d011, source_alpha ); glVertex3f ( x, y+h, z+h);
+                glColor4f ( d010, d010, d010, source_alpha ); glVertex3f ( x, y+h, z );
+                glColor4f ( d000, d000, d000, source_alpha ); glVertex3f ( x, y, z );
+                glColor4f ( d001, d001, d001, source_alpha ); glVertex3f ( x, y, z+h );
+                
+                glColor4f ( d100, d100, d100, source_alpha ); glVertex3f ( x+h, y, z );
+                glColor4f ( d000, d000, d000, source_alpha ); glVertex3f ( x, y, z );
+                glColor4f ( d001, d001, d001, source_alpha ); glVertex3f ( x, y, z+h );
+                glColor4f ( d101, d101, d101, source_alpha ); glVertex3f ( x+h, y, z+h );
+                
+                glColor4f ( d110, d110, d110, source_alpha ); glVertex3f ( x+h, y+h, z );
+                glColor4f ( d010, d010, d010, source_alpha ); glVertex3f ( x, y+h, z );
+                glColor4f ( d011, d011, d011, source_alpha ); glVertex3f ( x, y+h, z+h);
+                glColor4f ( d111, d111, d111, source_alpha ); glVertex3f ( x+h, y+h, z+h );
+            }
         }
     }
-    
     glEnd ();
 }
 
 static void draw_temperature (void)
 {
-    int i, j;
-    float x, y, h, d00, d01, d10, d11;
+    int i, j, k;
+    float x, y, z, h, d000, d010, d100, d110, d001, d011, d101, d111;;
     
     h = 1.0f/N;
     
     glBegin ( GL_QUADS );
     
-    for ( i=0 ; i<=N ; i++ ) {
+    for ( i=0; i<=N; i++ ) {
         x = (i-0.5f)*h;
-        for ( j=0 ; j<=N ; j++ ) {
+        for ( j=0; j<=N; j++ ) {
             y = (j-0.5f)*h;
-            
-            d00 = temp[IX(i,j)];
-            d01 = temp[IX(i,j+1)];
-            d10 = temp[IX(i+1,j)];
-            d11 = temp[IX(i+1,j+1)];
-            
-            glColor3f ( d00, d00, d00 ); glVertex2f ( x, y );
-            glColor3f ( d10, d10, d10 ); glVertex2f ( x+h, y );
-            glColor3f ( d11, d11, d11 ); glVertex2f ( x+h, y+h );
-            glColor3f ( d01, d01, d01 ); glVertex2f ( x, y+h );
+            for ( k=0; k<=N; k++ ) {
+                z = (k-0.5f)*h;
+                
+                d000 = temp[IX(i,j,k)];
+                d010 = temp[IX(i,j+1,k)];
+                d100 = temp[IX(i+1,j,k)];
+                d110 = temp[IX(i+1,j+1,k)];
+                
+                d001 = temp[IX(i,j,k+1)];
+                d011 = temp[IX(i,j+1,k+1)];
+                d101 = temp[IX(i+1,j,k+1)];
+                d111 = temp[IX(i+1,j+1,k+1)];
+                
+                
+                // draw density as a cube of quads (6 faces)
+                
+                glColor4f ( d111, 0, 100-d111, source_alpha ); glVertex3f ( x+h,y+h,z+h );
+                glColor4f ( d011, 0, 100-d011, source_alpha ); glVertex3f ( x, y+h, z+h);
+                glColor4f ( d001, 0, 100-d001, source_alpha ); glVertex3f ( x, y, z+h );
+                glColor4f ( d101, 0, 100-d101, source_alpha ); glVertex3f ( x+h, y, z+h );
+                
+                glColor4f ( d110, 0, 100-d110, source_alpha ); glVertex3f ( x+h, y+h, z );
+                glColor4f ( d111, 0, 100-d111, source_alpha ); glVertex3f ( x+h,y+h,z+h );
+                glColor4f ( d101, 0, 100-d101, source_alpha ); glVertex3f ( x+h, y, z+h );
+                glColor4f ( d100, 0, 100-d100, source_alpha ); glVertex3f ( x+h, y, z );
+                
+                glColor4f ( d010, 0, 100-d010, source_alpha ); glVertex3f ( x, y+h, z );
+                glColor4f ( d110, 0, 100-d110, source_alpha ); glVertex3f ( x+h, y+h, z );
+                glColor4f ( d100, 0, 100-d100, source_alpha ); glVertex3f ( x+h, y, z );
+                glColor4f ( d000, 0, 100-d000, source_alpha ); glVertex3f ( x, y, z );
+                
+                glColor4f ( d011, 0, 100-d011, source_alpha ); glVertex3f ( x, y+h, z+h);
+                glColor4f ( d010, 0, 100-d010, source_alpha ); glVertex3f ( x, y+h, z );
+                glColor4f ( d000, 0, 100-d000, source_alpha ); glVertex3f ( x, y, z );
+                glColor4f ( d001, 0, 100-d001, source_alpha ); glVertex3f ( x, y, z+h );
+                
+                glColor4f ( d100, 0, 100-d100, source_alpha ); glVertex3f ( x+h, y, z );
+                glColor4f ( d000, 0, 100-d000, source_alpha ); glVertex3f ( x, y, z );
+                glColor4f ( d001, 0, 100-d001, source_alpha ); glVertex3f ( x, y, z+h );
+                glColor4f ( d101, 0, 100-d101, source_alpha ); glVertex3f ( x+h, y, z+h );
+                
+                glColor4f ( d110, 0, 100-d110, source_alpha ); glVertex3f ( x+h, y+h, z );
+                glColor4f ( d010, 0, 100-d010, source_alpha ); glVertex3f ( x, y+h, z );
+                glColor4f ( d011, 0, 100-d011, source_alpha ); glVertex3f ( x, y+h, z+h);
+                glColor4f ( d111, 0, 100-d111, source_alpha ); glVertex3f ( x+h, y+h, z+h );
+            }
         }
     }
     
@@ -209,27 +285,28 @@ static void draw_temperature (void)
 
 static void get_from_UI ( float * d, float * u, float * v, float *t )
 {
-    int i, j, size = (N+2)*(N+2);
+    int i, j, k, size = (N+2)*(N+2)*(N+2);
     
     for ( i=0 ; i<size ; i++ ) {
-        u[i] = v[i] = d[i] = 0.0f;
+        u[i] = v[i] = d[i] = t[i] = 0.0f;
     }
     
     if ( !mouse_down[0] && !mouse_down[2] ) return;
     
     i = (int)((       mx /(float)win_x)*N+1);
     j = (int)(((win_y-my)/(float)win_y)*N+1);
+    k = (int) (N/2);
     
     if ( i<1 || i>N || j<1 || j>N ) return;
     
     if ( mouse_down[0] ) {
-        u[IX(i,j)] = force * (mx-omx);
-        v[IX(i,j)] = force * (omy-my);
+        u[IX(i,j,k)] = force * (mx-omx);
+        v[IX(i,j,k)] = force * (omy-my);
     }
     
     if ( mouse_down[2] ) {
-        d[IX(i,j)] = source;
-        t[IX(i,j)] = temp_source;
+        d[IX(i,j,k)] = source;
+        t[IX(i,j,k)] = temp_source;
     }
     
     omx = mx;
@@ -263,6 +340,21 @@ static void key_func ( unsigned char key, int x, int y )
         case 'V':
             dvel = !dvel;
             break;
+            
+        case 't':
+        case 'T':
+            temp_source = -temp_source;
+            break;
+            
+        case 'a':
+        case 'A':
+            dAxis = !dAxis;
+            break;
+            
+        case 'x':       // 'X' key - add source at centre
+            addsource = 1;
+            break;
+            
     }
 }
 
@@ -282,34 +374,88 @@ static void motion_func ( int x, int y )
 
 static void reshape_func ( int width, int height )
 {
-    glutSetWindow ( win_id );
-    glutReshapeWindow ( width, height );
-    
-    win_x = width;
-    win_y = height;
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0, (float)width/height, 0.001, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, -2.5f);
 }
 
 static void idle_func ( void )
 {
-    get_from_UI ( dens_prev, u_prev, v_prev, temp_prev );
-    vel_step ( N, u, v, temp, u_prev, v_prev, temp_prev, visc, dt);
-    dens_step ( N, dens, dens_prev, u, v, diff, dt );
-    temp_step(N, temp, temp_prev, u, v, diff, dt);
+        get_from_UI ( dens_prev, u_prev, v_prev, temp_prev );
+    
+    
+    vel_step ( N, u, v, w, temp, u_prev, v_prev, w_prev, temp_prev, visc, dt, buo);
+    dens_step ( N, dens, dens_prev, u, v, w, diff, dt );
+    temp_step(N, temp, temp_prev, u, v, w, diff/100, dt);
     
     glutSetWindow ( win_id );
     glutPostRedisplay ();
 }
 
-static void display_func ( void )
+static void pre_display ( void )
 {
-    pre_display ();
-    
-    if ( dvel ) draw_velocity ();
-    else		draw_density ();
-    
-    post_display ();
+    glViewport ( 0, 0, win_x, win_y );
+    glMatrixMode ( GL_PROJECTION );
+    glLoadIdentity ();
+    gluOrtho2D ( 0.0, 1.0, 0.0, 1.0 );
+    glClearColor ( 0.0f, 0.0f, 0.0f, 1.0f );
+    glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 }
 
+static void post_display ( void )
+{
+    glutSwapBuffers ();
+}
+
+static void display_func ( void )
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+
+    glRotatef(rot[0], 1.0f, 0.0f, 0.0f);
+    glRotatef(rot[1], 0.0f, 1.0f, 0.0f);
+    
+    if ( dvel ) draw_temperature ();
+    else		draw_density ();
+    
+    if (dAxis) draw_axis();
+    
+    glEnd();
+
+    glPopMatrix();
+    glFlush();
+    
+    post_display();
+}
+
+static void special_func(int key, int x, int y) {
+    
+    switch(key)
+    {
+        case GLUT_KEY_UP:
+            rot[0] += 5.0f;
+            rot[0] = clamp(rot[0]);
+            break;
+        case GLUT_KEY_DOWN:
+            rot[0] -= 5.0f;
+            rot[0] = clamp(rot[0]);
+            break;
+        case GLUT_KEY_LEFT:
+            rot[1] -= 5.0f;
+            rot[1] = clamp(rot[1]);
+            break;
+        case GLUT_KEY_RIGHT:
+            rot[1] += 5.0f;
+            rot[1] = clamp(rot[1]);
+            break;
+
+    }
+}
 
 /*
  ----------------------------------------------------------------------
@@ -323,7 +469,7 @@ static void open_glut_window ( void )
     
     glutInitWindowPosition ( 0, 0 );
     glutInitWindowSize ( win_x, win_y );
-    win_id = glutCreateWindow ( "Alias | wavefront" );
+    win_id = glutCreateWindow ( "3D Convection Fluids Simulation" );
     
     glClearColor ( 0.0f, 0.0f, 0.0f, 1.0f );
     glClear ( GL_COLOR_BUFFER_BIT );
@@ -331,14 +477,13 @@ static void open_glut_window ( void )
     glClear ( GL_COLOR_BUFFER_BIT );
     glutSwapBuffers ();
     
-    pre_display ();
-    
     glutKeyboardFunc ( key_func );
     glutMouseFunc ( mouse_func );
     glutMotionFunc ( motion_func );
     glutReshapeFunc ( reshape_func );
     glutIdleFunc ( idle_func );
     glutDisplayFunc ( display_func );
+    glutSpecialFunc(special_func);
 }
 
 
@@ -365,13 +510,13 @@ int main ( int argc, char ** argv )
     }
     
     if ( argc == 1 ) {
-        N = 64;
-        dt = 0.1f;
-        diff = 0.0001f;
+        N = 32;
+        dt = 0.4f;
+        diff = 0.00001f;
         visc = 0.0f;
-        force = 5.0f;
+        force = 0.0f;
         source = 100.0f;
-        temp_source = 100.0f;
+        temp_source = 50.0f;
         fprintf ( stderr, "Using defaults : N=%d dt=%g diff=%g visc=%g force = %g source=%g\n",
                  N, dt, diff, visc, force, source );
     } else {
@@ -392,13 +537,20 @@ int main ( int argc, char ** argv )
     printf ( "\t Quit by pressing the 'q' key\n" );
     
     dvel = 0;
+    rot[0] = 30;
+    rot[1] = -45;
     
     if ( !allocate_data () ) exit ( 1 );
-    clear_data ();
+    clear_data ();    
     
-    win_x = 512;
-    win_y = 512;
+    win_x = 768;
+    win_y = 768;
     open_glut_window ();
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+    
+    glEnable(GL_ALPHA_TEST);
+    glAlphaFunc(GL_GREATER, 0);
     
     glutMainLoop ();
     
--- fluid_demo/solver.c
+++ fluid_demo/solver.c
@@ -1,130 +1,163 @@
-#define IX(i,j) ((i)+(N+2)*(j))
+#define IX(i,j,k) ((i)+(N+2)*(j)+(N+2)*(N+2)*(k))
 #define SWAP(x0,x) {float * tmp=x0;x0=x;x=tmp;}
-#define FOR_EACH_CELL for ( i=1 ; i<=N ; i++ ) { for ( j=1 ; j<=N ; j++ ) {
-#define END_FOR }}
+#define FOR_EACH_CELL for ( i=1 ; i<=N ; i++ ) { for ( j=1 ; j<=N ; j++ ) { for (k=1;k<=N;k++) {
+#define END_FOR }}}
 
 void add_source ( int N, float * x, float * s, float dt )
 {
-	int i, size=(N+2)*(N+2);
+	int i, size=(N+2)*(N+2)*(N+2);
 	for ( i=0 ; i<size ; i++ ) x[i] += dt*s[i];
 }
 
 void set_bnd ( int N, int b, float * x )
 {
-	int i;
-
-	for ( i=1 ; i<=N ; i++ ) {
-		x[IX(0  ,i)] = b==1 ? -x[IX(1,i)] : x[IX(1,i)];
-		x[IX(N+1,i)] = b==1 ? -x[IX(N,i)] : x[IX(N,i)];
-		x[IX(i,0  )] = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
-		x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)];
-	}
-	x[IX(0  ,0  )] = 0.5f*(x[IX(1,0  )]+x[IX(0  ,1)]);
-	x[IX(0  ,N+1)] = 0.5f*(x[IX(1,N+1)]+x[IX(0  ,N)]);
-	x[IX(N+1,0  )] = 0.5f*(x[IX(N,0  )]+x[IX(N+1,1)]);
-	x[IX(N+1,N+1)] = 0.5f*(x[IX(N,N+1)]+x[IX(N+1,N)]);
+    int i, j;
+    
+    for ( i=1 ; i<=N ; i++ ) {
+        for ( j=1 ; j<=N ; j++ ) {
+            x[IX(i,j,0 )] = b==3 ? -x[IX(i,j,1)] : x[IX(i,j,1)];
+            x[IX(i,j,N+1)] = b==3 ? -x[IX(i,j,N)] : x[IX(i,j,N)];
+        }
+    }
+    
+    for ( i=1 ; i<=N ; i++ ) {
+        for ( j=1 ; j<=N ; j++ ) {
+            x[IX(0  ,i, j)] = b==1 ? -x[IX(1,i,j)] : x[IX(1,i,j)];
+            x[IX(N+1,i, j)] = b==1 ? -x[IX(N,i,j)] : x[IX(N,i,j)];
+        }
+    }
+    
+    for ( i=1 ; i<=N ; i++ ) {
+        for ( j=1 ; j<=N ; j++ ) {
+            x[IX(i,0,j )] = b==2 ? -x[IX(i,1,j)] : x[IX(i,1,j)];
+            x[IX(i,N+1,j)] = b==2 ? -x[IX(i,N,j)] : x[IX(i,N,j)];
+        }
+    }
+    
+    x[IX(0  ,0, 0  )] = 1.0/3.0*(x[IX(1,0,0  )]+x[IX(0  ,1,0)]+x[IX(0 ,0,1)]);
+    x[IX(0  ,N+1, 0)] = 1.0/3.0*(x[IX(1,N+1, 0)]+x[IX(0  ,N, 0)] + x[IX(0  ,N+1, 1)]);
+    
+    x[IX(N+1,0, 0 )] = 1.0/3.0*(x[IX(N,0,0  )]+x[IX(N+1,1,0)] + x[IX(N+1,0,1)]) ;
+    x[IX(N+1,N+1,0)] = 1.0/3.0*(x[IX(N,N+1,0)]+x[IX(N+1,N,0)]+x[IX(N+1,N+1,1)]);
+    
+    x[IX(0  ,0, N+1 )] = 1.0/3.0*(x[IX(1,0,N+1  )]+x[IX(0  ,1,N+1)]+x[IX(0 ,0,N)]);
+    x[IX(0  ,N+1, N+1)] = 1.0/3.0*(x[IX(1,N+1, N+1)]+x[IX(0  ,N, N+1)] + x[IX(0  ,N+1, N)]);
+    
+    x[IX(N+1,0, N+1 )] = 1.0/3.0*(x[IX(N,0,N+1  )]+x[IX(N+1,1,N+1)] + x[IX(N+1,0,N)]) ;
+    x[IX(N+1,N+1,N+1)] = 1.0/3.0*(x[IX(N,N+1,N+1)]+x[IX(N+1,N,N+1)]+x[IX(N+1,N+1,N)]);
 }
 
 void lin_solve ( int N, int b, float * x, float * x0, float a, float c )
 {
-	int i, j, k;
+	int i, j, k, l;
 
-	for ( k=0 ; k<20 ; k++ ) {
+	for ( l=0 ; l<10 ; l++ ) {
 		FOR_EACH_CELL
-			x[IX(i,j)] = (x0[IX(i,j)] + a*(x[IX(i-1,j)]+x[IX(i+1,j)]+x[IX(i,j-1)]+x[IX(i,j+1)]))/c;
-		END_FOR
+			x[IX(i,j,k)] = (x0[IX(i,j,k)] + a*(x[IX(i-1,j,k)]+x[IX(i+1,j,k)]+x[IX(i,j-1,k)]+x[IX(i,j+1,k)]+x[IX(i,j,k-1)]+x[IX(i,j,k+1)]))/c;
+        END_FOR
 		set_bnd ( N, b, x );
 	}
 }
 
 void diffuse ( int N, int b, float * x, float * x0, float diff, float dt )
 {
-	float a=dt*diff*N*N;
-	lin_solve ( N, b, x, x0, a, 1+4*a );
+	float a=dt*diff*N*N*N;
+	lin_solve ( N, b, x, x0, a, 1+6*a );
 }
 
-void advect ( int N, int b, float * d, float * d0, float * u, float * v, float dt )
+void advect ( int N, int b, float * d, float * d0, float * u, float * v, float * w, float dt )
 {
-	int i, j, i0, j0, i1, j1;
-	float x, y, s0, t0, s1, t1, dt0;
+	int i, j, k, i0, j0, i1, j1, k0, k1;
+	float x, y, z, s0, t0, u0, s1, t1, u1, dt0;
 
 	dt0 = dt*N;
 	FOR_EACH_CELL
-		x = i-dt0*u[IX(i,j)]; y = j-dt0*v[IX(i,j)];
-		if (x<0.5f) x=0.5f; if (x>N+0.5f) x=N+0.5f; i0=(int)x; i1=i0+1;
-		if (y<0.5f) y=0.5f; if (y>N+0.5f) y=N+0.5f; j0=(int)y; j1=j0+1;
-		s1 = x-i0; s0 = 1-s1; t1 = y-j0; t0 = 1-t1;
-		d[IX(i,j)] = s0*(t0*d0[IX(i0,j0)]+t1*d0[IX(i0,j1)])+
-					 s1*(t0*d0[IX(i1,j0)]+t1*d0[IX(i1,j1)]);
+        x = i-dt0*u[IX(i,j,k)];
+        y = j-dt0*v[IX(i,j,k)];
+        z = k-dt0*w[IX(i,j,k)];
+        if (x<0.5f) x=0.5f; if (x>N+0.5f) x=N+0.5f; i0=(int)x; i1=i0+1;
+        if (y<0.5f) y=0.5f; if (y>N+0.5f) y=N+0.5f; j0=(int)y; j1=j0+1;
+        if (z<0.5f) z=0.5f; if (z>N+0.5f) z=N+0.5f; k0=(int)z; k1=k0+1;
+        
+        s1 = x-i0; s0 = 1-s1; t1 = y-j0; t0 = 1-t1; u1 = z-k0; u0 = 1-u1;
+        d[IX(i,j,k)] = s0*(t0*u0*d0[IX(i0,j0,k0)]+t1*u0*d0[IX(i0,j1,k0)]+t0*u1*d0[IX(i0,j0,k1)]+t1*u1*d0[IX(i0,j1,k1)])+ s1*(t0*u0*d0[IX(i1,j0,k0)]+t1*u0*d0[IX(i1,j1,k0)]+t0*u1*d0[IX(i1,j0,k1)]+t1*u1*d0[IX(i1,j1,k1)]);
 	END_FOR
 	set_bnd ( N, b, d );
 }
 
 void buoyancy (int N, float* fBuoy, float* temp0)
 {
-    int i,j;
+    int i,j,k;
     float tamb = 0.0;
     float a = 0.000625f;
     float b = 0.025f;
     
     FOR_EACH_CELL
-        tamb += fBuoy[IX(i,j)];
+        tamb += fBuoy[IX(i,j,k)];
     END_FOR
     
     // get average temperature
-    
     tamb /=(N*N);
+    
     FOR_EACH_CELL
-        fBuoy[IX(i,j)] = a * temp0[IX(i,j)] - b * (temp0[IX(i,j)] - tamb);
+        fBuoy[IX(i,j,k)] = -a * temp0[IX(i,j,k)] + b * (temp0[IX(i,j,k)] - tamb);
     END_FOR
 }
 
-void project ( int N, float * u, float * v, float * p, float * div )
+void project ( int N, float * u, float * v, float * w, float * p, float * div )
 {
-	int i, j;
+	int i, j, k;
 
 	FOR_EACH_CELL
-		div[IX(i,j)] = -0.5f*(u[IX(i+1,j)]-u[IX(i-1,j)]+v[IX(i,j+1)]-v[IX(i,j-1)])/N;
-		p[IX(i,j)] = 0;
-	END_FOR	
+		div[IX(i,j,k)] = -1.0/3.0*((u[IX(i+1,j,k)]-u[IX(i-1,j,k)])/N+(v[IX(i,j+1,k)]-v[IX(i,j-1,k)])/N+(w[IX(i,j,k+1)]-w[IX(i,j,k-1)])/N);
+        p[IX(i,j,k)] = 0;
+	END_FOR
+    
 	set_bnd ( N, 0, div ); set_bnd ( N, 0, p );
 
-	lin_solve ( N, 0, p, div, 1, 4 );
+	lin_solve ( N, 0, p, div, 1, 6 );
 
 	FOR_EACH_CELL
-		u[IX(i,j)] -= 0.5f*N*(p[IX(i+1,j)]-p[IX(i-1,j)]);
-		v[IX(i,j)] -= 0.5f*N*(p[IX(i,j+1)]-p[IX(i,j-1)]);
+        u[IX(i,j,k)] -= 0.5f*N*(p[IX(i+1,j,k)]-p[IX(i-1,j,k)]);
+        v[IX(i,j,k)] -= 0.5f*N*(p[IX(i,j+1,k)]-p[IX(i,j-1,k)]);
+        w[IX(i,j,k)] -= 0.5f*N*(p[IX(i,j,k+1)]-p[IX(i,j,k-1)]);
 	END_FOR
-	set_bnd ( N, 1, u ); set_bnd ( N, 2, v );
+    
+    set_bnd ( N, 1, u ); set_bnd ( N, 2, v ); set_bnd(N,3,w);
 }
 
-void dens_step ( int N, float * x, float * x0, float * u, float * v, float diff, float dt )
+void dens_step ( int N, float * x, float * x0, float * u, float * v, float * w, float diff, float dt )
 {
 	add_source ( N, x, x0, dt );
 	SWAP ( x0, x ); diffuse ( N, 0, x, x0, diff, dt );
-	SWAP ( x0, x ); advect ( N, 0, x, x0, u, v, dt );
+	SWAP ( x0, x ); advect ( N, 0, x, x0, u, v, w, dt );
 }
 
-void temp_step ( int N, float * x, float * x0, float * u, float * v, float diff, float dt )
+void temp_step ( int N, float * x, float * x0, float * u, float * v, float * w, float diff, float dt )
 {
     add_source ( N, x, x0, dt );
     SWAP ( x0, x ); diffuse ( N, 0, x, x0, diff, dt );
-    SWAP ( x0, x ); advect ( N, 0, x, x0, u, v, dt );
+    SWAP ( x0, x ); advect ( N, 0, x, x0, u, v, w, dt );
 }
 
-void vel_step ( int N, float * u, float * v, float * temp, float * u0, float * v0, float * temp0, float visc, float dt )
+void vel_step ( int N, float * u, float * v, float * w, float * temp, float * u0, float * v0, float * w0, float * temp0, float visc, float dt, float* buo )
 {
-    add_source ( N, u, u0, dt ); add_source ( N, v, v0, dt );
+    add_source ( N, u, u0, dt ); add_source ( N, v, v0, dt ); add_source( N, w, w0, dt);
     
-    //buoyancy
-    buoyancy(N, v0, temp0); add_source(N, v, v0, dt);
+    // buoyancy
+    buoyancy(N, buo, temp0); add_source(N, v, buo, dt);
     
     
 	SWAP ( u0, u ); diffuse ( N, 1, u, u0, visc, dt );
 	SWAP ( v0, v ); diffuse ( N, 2, v, v0, visc, dt );
-	project ( N, u, v, u0, v0 );
-	SWAP ( u0, u ); SWAP ( v0, v );
-	advect ( N, 1, u, u0, u0, v0, dt ); advect ( N, 2, v, v0, u0, v0, dt );
-	project ( N, u, v, u0, v0 );
+    SWAP ( w0, w ); diffuse ( N, 3, w, w0, visc, dt );
+	project ( N, u, v, w, u0, v0);
+    
+	SWAP ( u0, u ); SWAP ( v0, v ); SWAP ( w0, w );
+    
+	advect ( N, 1, u, u0, u0, v0, w0, dt );
+    advect ( N, 2, v, v0, u0, v0, w0, dt );
+    advect ( N, 3, w, w0, u0, v0, w0, dt );
+	project ( N, u, v, w, u0, v0 );
 }
 
